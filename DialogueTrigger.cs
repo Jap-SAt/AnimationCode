04bdd0c26bbb3c9240e0c9753cd0c66b8c1d61ec
Jap-SAt <japsuperdog527@gmail.com>
2025-03-16 13:02:19 +0700

    Another version

---
+++ DialogueTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueCharacter
+{
+    public string name;
+    public Sprite icon;
+}
+
+[System.Serializable]
+public class DialogueLine
+{
+    public DialogueCharacter character;
+    [TextArea(3, 10)]
+    public string line;
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    public List<DialogueLine> dialogue = new List<DialogueLine>();
+    internal IEnumerable<DialogueLine> dialogueLines;
+}
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+}
